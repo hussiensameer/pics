1c8f715f89262202fe587bf2fb6e4df7fb212b0a
hussien sameer ahmed <hsa20st@gmail.com>
2023-10-20 14:52:09 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,133 @@
+onEvent("image1", "click", function( ) {
+  playSpeech("المطبخ", "female", "العربية");
+  setScreen("screen1");
+});
+onEvent("image2", "click", function( ) {
+  playSpeech("الغرفة", "female", "العربية");
+  setScreen("screen2");
+});
+onEvent("image3", "click", function( ) {
+  playSpeech("الحمام", "female", "العربية");
+  setScreen("screen3");
+});
+onEvent("image4", "click", function( ) {
+  playSpeech("الصالة", "female", "العربية");
+  setScreen("screen4");
+});
+onEvent("image5", "click", function( ) {
+  playSpeech("الكراج", "female", "العربية");
+  setScreen("screen5");
+});
+onEvent("image6", "click", function( ) {
+  playSpeech("الحديقة", "female", "العربية");
+  setScreen("screen6");
+});
+onEvent("image7", "click", function( ) {
+  playSpeech("الحريطة", "female", "العربية");
+  setScreen("screen7");
+});
+onEvent("image8", "click", function( ) {
+  playSpeech("الشارع", "female", "العربية");
+  setScreen("screen8");
+});
+onEvent("image9", "click", function( ) {
+  playSpeech("الدرج", "female", "العربية");
+  setScreen("screen9");
+});
+onEvent("image10", "click", function( ) {
+  playSpeech("المرسم", "female", "العربية");
+  setScreen("screen10");
+});
+onEvent("image11", "click", function( ) {
+  playSpeech("الخدمات", "female", "العربية");
+  setScreen("screen11");
+});
+
+
+
+
+onEvent("image12", "click", function( ) {
+  playSpeech("المطعم", "female", "العربية");
+  setScreen("screen11");
+});
+onEvent("image13", "click", function( ) {
+  playSpeech("لحم", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image14", "click", function( ) {
+  playSpeech("عصير", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image15", "click", function( ) {
+  playSpeech("سمك", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image16", "click", function( ) {
+  playSpeech("كيك", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image17", "click", function( ) {
+  playSpeech("ماء", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image18", "click", function( ) {
+  playSpeech("خضروات", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image19", "click", function( ) {
+  playSpeech("فواكه", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image20", "click", function( ) {
+  playSpeech("طباخ", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image21", "click", function( ) {
+  playSpeech("ثلاجة", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image22", "click", function( ) {
+  playSpeech("العودة", "female", "العربية");
+  setScreen("screen0");
+});
+onEvent("image23", "click", function( ) {
+  playSpeech("طعام", "female", "العربية");
+  setScreen("screen0");
+});
+
+
+
+onEvent("image24", "click", function( ) {
+  playSpeech("سرير", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image25", "click", function( ) {
+  playSpeech("دولاب", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image26", "click", function( ) {
+  playSpeech("بنطرون", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image27", "click", function( ) {
+  playSpeech("تشيرت", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image28", "click", function( ) {
+  playSpeech("هاتف", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image29", "click", function( ) {
+  playSpeech("المكيف", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image30", "click", function( ) {
+  playSpeech("مرااه", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image31", "click", function( ) {
+  playSpeech("مشط", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image32", "click", function( ) {
+  playSpeech("منشفة", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image34", "click", function( ) {
+  playSpeech("الواجهه الرئيسية", "female", "العربية");
+  setScreen("screen0");
+});onEvent("image35", "click", function( ) {
+  playSpeech("كتاب ", "female", "العربية");
+  setScreen("screen0");
+});
